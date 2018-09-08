e8dd690e31abdc3c59c1b5bb4999b31146b52615
Dariusz Kwiatkowski <ktoztam@gmail.com>
2018-09-08 22:39:32 +0200

    Added new architecture, added tags to records

--- parser.py
+++ parser.py
@@ -1,23 +1,29 @@
 import os
 import re
 import json
-scripts_dir = "scripts"
-scripts = os.listdir(scripts_dir)
+scripts_dirs = ["scripts/gothic_1", "scripts/gothic_2"]
+scripts_sets = [os.listdir(d) for d in scripts_dirs]
+scripts_labels = ["1", "2"]
 assigments = []
 
-for script in scripts:
-    with open(scripts_dir + "/" + script, 'r', encoding="Windows-1250") as file:
-        for line in file:
-            line = line.strip()
-            if "AI_Output" in line and "//" != line.strip()[:2]:
-                sound_file = re.split(",|//", line)[2].strip()[1:-3]
-                sound_text = line.split("//")[1]
-                assigments.append({
-                    "filename": sound_file,
-                    "text": sound_text
-                })
+for i, scripts in enumerate(scripts_sets):
+    print("parsing " + scripts_dirs[i])
+    for script in scripts:
+        with open(scripts_dirs[i] + "/" + script, 'r', encoding="Windows-1250") as file:
+            for line in file:
+                line = line.strip()
+                if "AI_Output" in line and "//" != line.strip()[:2]:
+                    sound_file = re.split(",|//", line)[2].strip()[1:-3]
+                    sound_text = line.split("//")[1]
+                    assigments.append({
+                        "filename": sound_file,
+                        "text": sound_text,
+                        "g": scripts_labels[i],
+                        "source": file.name.split("/")[-1][:-2]
+                    })
 
 with open("sound_text.json", "w", encoding="utf-8") as file:
     json.dump(assigments, file, ensure_ascii=False)
+    print("success")
 
 
